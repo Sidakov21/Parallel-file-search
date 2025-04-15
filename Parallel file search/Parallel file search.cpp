4e8c1d9d0068f3671b698c785ce842fa93099883
Sidakov_Amir <ascn_1@icloud.com>
2025-04-15 12:18:47 +0300

    Этап 2: Обход директорий и обработка всех файлов

--- Parallel file search/Parallel file search.cpp
+++ Parallel file search/Parallel file search.cpp
@@ -1,21 +1,14 @@
-﻿#include <iostream>
+﻿#include <windows.h>
+#include <stdio.h>
 
 #define MAX_LINE_LENGTH 1024
 
-int main(int argc, char *argv[])
-{
-    if (argc != 3) {
-        fprintf(stderr, "Usage: %s <filename> <substring>\n", argv[0]);
-        return 1;
-    }
 
-    const char* filename = argv[1];
-    const char* substring = argv[2];
-
+void search_in_file(const char* filepath, const char* substring) {
     FILE* file = NULL;
-    if (fopen_s(&file, filename, "r") != 0) {
-        perror("Error opening file");
-        return 1;
+    if (fopen_s(&file, filepath, "r") != 0 || file == NULL) {
+        fprintf(stderr, "Error opening file: %s\n", filepath);
+        return;
     }
 
     char line[MAX_LINE_LENGTH];
@@ -23,12 +16,57 @@ int main(int argc, char *argv[])
 
     while (fgets(line, sizeof(line), file)) {
         if (strstr(line, substring)) {
-            printf("[MATCH] %s (line %d): %s", filename, line_number, line);
+            printf("[MATCH] %s (line %d): %s \n\n", filepath, line_number, line);
         }
         line_number++;
     }
 
     fclose(file);
-    return 0;
+}
 
+
+void search_directory(const char* dirpath, const char* substring) {
+    char search_path[MAX_PATH];
+    snprintf(search_path, MAX_PATH, "%s\\*", dirpath);
+
+    WIN32_FIND_DATAA fd;
+    HANDLE hFind = FindFirstFileA(search_path, &fd);
+
+    if (hFind == INVALID_HANDLE_VALUE) return;
+
+    do
+    {
+        if (strcmp(fd.cFileName, ".") == 0 || strcmp(fd.cFileName, "..") == 0)
+            continue;
+
+        char full_path[MAX_PATH];
+        snprintf(full_path, MAX_PATH, "%s\\%s", dirpath, fd.cFileName);
+
+        if (fd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) {
+            search_directory(full_path, substring);  // рекурсивный вызов
+        }
+        else {
+            // Проверка на расширение .txt
+            const char* ext = strrchr(fd.cFileName, '.');
+            if (ext && strcmp(ext, ".txt") == 0) {
+                search_in_file(full_path, substring);
+            }
+        }
+
+    } while (FindNextFileA(hFind, &fd));
+}
+
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3) {
+        printf("Usage: %s <directory> <substring>\n", argv[0]);
+        return 1;
+    }
+
+    const char* dirpath = argv[1];
+    const char* substring = argv[2];
+
+    search_directory(dirpath, substring);
+    return 0;
 }
