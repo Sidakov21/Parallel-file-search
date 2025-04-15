616e1b09358659b2f30c270bd12ef7e0bbd349e8
Sidakov_Amir <ascn_1@icloud.com>
2025-04-15 11:14:32 +0300

    Этап 1: Поиск подстроки в одном файле (без многопоточности)

--- Parallel file search/Parallel file search.cpp
+++ Parallel file search/Parallel file search.cpp
@@ -1,6 +1,34 @@
 ﻿#include <iostream>
 
-int main()
+#define MAX_LINE_LENGTH 1024
+
+int main(int argc, char *argv[])
 {
-    std::cout << "Hello World!\n";
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s <filename> <substring>\n", argv[0]);
+        return 1;
+    }
+
+    const char* filename = argv[1];
+    const char* substring = argv[2];
+
+    FILE* file = NULL;
+    if (fopen_s(&file, filename, "r") != 0) {
+        perror("Error opening file");
+        return 1;
+    }
+
+    char line[MAX_LINE_LENGTH];
+    int line_number = 1;
+
+    while (fgets(line, sizeof(line), file)) {
+        if (strstr(line, substring)) {
+            printf("[MATCH] %s (line %d): %s", filename, line_number, line);
+        }
+        line_number++;
+    }
+
+    fclose(file);
+    return 0;
+
 }
